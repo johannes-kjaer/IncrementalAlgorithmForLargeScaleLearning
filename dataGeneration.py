2dc3deaaca9c02fdf1273ca1579a61ec291b7a81
Johannes Kjaer <77483678+johannes-kjaer@users.noreply.github.com>
2023-12-02 11:51:19 +0100

    Made function for generating randommultivariate normally distributed data

---
+++ dataGeneration.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generateRandomCovMatrix(n):
+    rng = np.random.default_rng()
+    A = rng.standard_normal((n,n))  
+    #A = np.random.rand(n, n)
+    return A @ A.T
+
+def generateRandomData(n, samples, ratio, mean = None, cov = None):
+    ''' Generates some random data with either zero or otherwise specified mean, with either a random covariance matrix (with the possibility of making it more or les correleated) or a specified one.
+    Args:
+        n: The number of variables
+        samples: The number of samples to be generated
+        ratio: The ratio between the random covariance and the identity matrix
+        mean: (n) array of the mean of the distribution
+        cov: (n,n) array of the covariance of the distribution
+    Returns:
+        data: (n,samples) array of the randomly generated data
+        mean: (n) array of the mean of the distribution
+        cov: (n,n) array of the covariance of the distribution
+    '''
+    if mean == None:
+        mean = np.zeros(n,dtype=np.double)
+
+    if cov == None:
+        cov = generateRandomCovMatrix(n) + ratio * np.identity(n,dtype=np.double)
+
+    randomData = np.random.multivariate_normal(mean, cov, samples)
+
+    return randomData, cov, mean
