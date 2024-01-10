b03539c716990906e8eb15a0b4709176f4a19ced
Johannes Kjaer <77483678+johannes-kjaer@users.noreply.github.com>
2024-01-10 11:21:16 +0100

    Data generation finished

--- newDataGeneration.py
+++ newDataGeneration.py
@@ -1,37 +1,57 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import ortho_group
 
-def covMatrix(dim,corr):
+def covMatrix(dim,c1,c2):
     ''' Makes a covariance matrix of size dim, with correlation parameter corr
     Args:
         dim: Int, number of dimensions of the data to generate
-        corr: Float, positive number. If zero the data is uncorrelated, the larger the value, the more correlated the data gets.
+        c1: Float, positive number
+        c2:
     Returns:
-        (dim, dim) covariance matrix array'''
+        (dim, dim) covariance matrix as numpy array
+    '''
     V = ortho_group.rvs(dim=dim) # Makes a matrix of linearly independent vectors to use as eigenvectors
-    L = np.diag(1,np.arange(dim+1))**corr # Makes a diagonal matrix of ordered values to use as eigevalues. To the power of corr to make the axes more or less correlated
-    return V @ L @ np.inverse(V) # Assembles the covariance matrix from its eigendecomposition
+    L = np.diag(np.arange(1,dim+1)*c2)**c1 / c2 # Makes a diagonal matrix of ordered values to use as eigenvalues, multiplies them by c2 to uniformly the variances along the eigenvectors. To the power of c2 to make the data more correlated with some eigenvectors than others
+    return V @ L @ np.linalg.inv(V), V, L # Assembles and the covariance matrix from its eigendecomposition, as well as V and L
 
 def is_pos_def(x):
     if np.all(np.linalg.eigvals(x) >= 0) == False:
         raise Exception('The covariance matrix is not positive semi-definite')
 
-def newDataGeneration(n_samples, dim, corr):
+def mulitnormalRandomData(n_samples, dim, c1, c2):
     ''' Generates random data
     Args:
         n_samples: Int, the number of data points to draw
         dim: Int, dimension of the dataspace
-        corr: Float, positive number. Zero gives uncorrelated data, large values gives higly correlated data.
+        c1: Float, positive number
+        c2:
     Returns:
         data: (n_samples,dim) array of the drawn data points
         Sigma: The covariance matrix of the generated data
     '''
-    Sigma = covMatrix(dim, corr)
+    Sigma, V, L = covMatrix(dim, c1, c2)
     is_pos_def(Sigma)
 
-    data = np.random.multivariate_normal(np.zeros(dim), np.identity(dim), n_samples)
-    return data, Sigma
-
+    data = np.random.multivariate_normal(np.zeros(dim), Sigma, n_samples)
+    return data, Sigma, V, L
     
+def display_data(data, eigVecs, eigVals):
+    fig, ax = plt.subplots()
+
+    ax.scatter(data[:,0],data[:,1])
+
+    origin = np.zeros(2)
+    ax.quiver(origin, origin, eigVecs[0,:] * eigVals, eigVecs[1,:] * eigVals)
+    plt.show()
+
+
+def testDataGeneration():
+    data, cov, V, L = mulitnormalRandomData(1000,2,3,2)
+    eVals = np.diag(L)
+
+    print(cov)
+    
+    display_data(data, V, eVals)
 
